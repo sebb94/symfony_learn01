d45a4bf48ef9cea63c49e9e61cfcae40b3411ae8
Seba <omgl0lwth@gmail.com>
2019-11-10 09:14:35 +0100

    51. Service configuration

--- src/Services/MyService.php
+++ src/Services/MyService.php
@@ -1,9 +1,9 @@
 <?php 
 
 namespace App\Services;
-
+use App\Services\MySecondService;
 class MyService{
-    public function __construct($second_service){
-        dump($second_service);
+    public function __construct(MySecondService $second_service){
+        dump('1st service');
     }
 }
