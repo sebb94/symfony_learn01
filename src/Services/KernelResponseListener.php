1282a647d54e7a5c66e47da98de679ed45ee0025
Seba <omgl0lwth@gmail.com>
2019-11-15 16:49:29 +0100

    67. Create listener

---
+++ src/Services/KernelResponseListener.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Services;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
+
+class KernelResponseListener{
+
+    public function onKernelResponse(FilterResponseEvent $event){
+
+        $response = new Response('123123');
+        $event->setResponse($response);
+
+    }
+}
