62e150c93f3baf40c4ca918fa07e17f785bb83e2
Seba <omgl0lwth@gmail.com>
2019-11-16 09:03:29 +0100

    72. Form - create a class

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -27,6 +27,11 @@ class Video
      */
     private $user;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +57,18 @@ class Video
     public function setUser(?User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
 
         return $this;
     }
